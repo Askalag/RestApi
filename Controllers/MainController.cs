c70e37353ccbc0b7dc974c696affca181e234cce
askalag <askalag@gmail.com>
2020-11-22 20:13:17 +0300

    initial commit

---
+++ Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MainController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public async Task<IActionResult> Hello()
+        {
+            var res = await Task.Run(() => "Hi! I'm working");
+            return Ok(res);
+        }
+    }
+}
