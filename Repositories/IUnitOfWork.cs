bf786239a055b2295852bc4f99bcf28f88a7bad0
askalag <askalag@gmail.com>
2020-11-24 22:37:03 +0300

    Add Repository out of work Pattern

---
+++ Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using RestApi.Models;
+using RestApi.Models.Cars;
+
+namespace RestApi.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Car> Customers { get; }
+        IRepository<Inspection> Orders { get; }
+        void Commit();
+    }
+}
